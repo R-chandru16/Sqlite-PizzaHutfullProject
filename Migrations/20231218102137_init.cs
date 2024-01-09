ad02fd120624cc28f725ee9a4882693364bd99bb
ChandruR <rchandru.rajendiran@gmail.com>
2024-01-09 20:12:14 +0530

    push

---
+++ Migrations/20231218102137_init.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PizzaHut.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: true),
+                    Address = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pizza",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Speicality = table.Column<string>(type: "TEXT", nullable: true),
+                    Crust = table.Column<string>(type: "TEXT", nullable: true),
+                    Images = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pizza", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Toppings",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Toppings", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Order_ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Pizza_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Qty = table.Column<int>(type: "INTEGER", nullable: false),
+                    OrderDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Order_ID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Customers_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Customers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Orders_Pizza_Pizza_ID",
+                        column: x => x.Pizza_ID,
+                        principalTable: "Pizza",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Order_ID = table.Column<int>(type: "INTEGER", nullable: false),
+                    ToppingsID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_Order_ID",
+                        column: x => x.Order_ID,
+                        principalTable: "Orders",
+                        principalColumn: "Order_ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Toppings_ToppingsID",
+                        column: x => x.ToppingsID,
+                        principalTable: "Toppings",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "ID", "Address", "Name", "Password", "Phone", "UserID" },
+                values: new object[,]
+                {
+                    { 101, "#Neyveli", "chandru", "chandru2598", "9876543213", "chandru@gmail.com" },
+                    { 102, "#demo", "demo", "demo", "9876543214", "Demo@gmail.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pizza",
+                columns: new[] { "ID", "Crust", "Description", "Images", "Name", "Price", "Speicality" },
+                values: new object[,]
+                {
+                    { 1001, "Fresh Pan Pizza", "Peppy Paneer Cheese Burst Topped with Extra Cheese", "/images/pizza-1.jpg", "Veg Loaded", 199.0, "with Pepse" },
+                    { 1002, "Fresh Pan Pizza", "Peppy Paneer Cheese Burst Topped with Extra Cheese", "/images/pizza-2.jpg", "Peppy Paneer Pizza", 299.0, "with extra toppings" },
+                    { 1003, "Fresh Pan Pizza", "Mashroon Topped", "/images/pizza-3.jpg", "Peper Chicken", 199.0, "with Pepse" },
+                    { 1004, "Fresh Pan Pizza", "Peppy Paneer Cheese Burst Topped with Extra Cheese", "/images/pizza-4.jpg", "Non Veg Loaded", 299.0, "with Pepse" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Toppings",
+                columns: new[] { "ID", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 201, "Tomato", 99.0 },
+                    { 202, "Cheese", 89.0 },
+                    { 203, "Onion", 100.0 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_Order_ID",
+                table: "OrderDetails",
+                column: "Order_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_ToppingsID",
+                table: "OrderDetails",
+                column: "ToppingsID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_Pizza_ID",
+                table: "Orders",
+                column: "Pizza_ID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_UserID",
+                table: "Orders",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Toppings");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Pizza");
+        }
+    }
+}
---
+++ Models/PizzaHutContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaHut.Models
+{
+    public class PizzaHutContext : DbContext
+    {
+        public PizzaHutContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Customers { get; set; }
+        public DbSet<Pizza> Pizza { get; set; }
+        public DbSet<Toppings> Toppings { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().HasData(
+                new Users() { ID = 101, Name = "chandru", UserID = "chandru@gmail.com", Password = "chandru2598", Phone = "9876543213", Address = "#Neyveli" },
+
+                 new Users() { ID = 102, Name = "demo", UserID = "Demo@gmail.com", Password = "demo", Phone = "9876543214", Address = "#demo" }
+                );
+            modelBuilder.Entity<Pizza>().HasData(
+               new Pizza() { ID = 1001, Name = "Veg Loaded", Price = 199, Speicality = "with Pepse", Crust = "Fresh Pan Pizza", Description = "Peppy Paneer Cheese Burst Topped with Extra Cheese", Images = "/images/pizza-1.jpg" },
+                new Pizza() { ID = 1002, Name = "Peppy Paneer Pizza", Price = 299, Speicality = "with extra toppings", Crust = "Fresh Pan Pizza", Description = "Peppy Paneer Cheese Burst Topped with Extra Cheese", Images = "/images/pizza-2.jpg" },
+                new Pizza() { ID = 1003, Name = "Peper Chicken", Price = 199, Speicality = "with Pepse", Crust = "Fresh Pan Pizza", Description = "Mashroon Topped", Images = "/images/pizza-3.jpg" },
+                 new Pizza() { ID = 1004, Name = "Non Veg Loaded", Price = 299, Speicality = "with Pepse", Crust = "Fresh Pan Pizza", Description = "Peppy Paneer Cheese Burst Topped with Extra Cheese", Images = "/images/pizza-4.jpg" }
+                );
+            modelBuilder.Entity<Toppings>().HasData(
+                new Toppings() { ID = 201, Name = "Tomato", Price = 99 },
+                new Toppings() { ID = 202, Name = "Cheese", Price = 89 },
+                new Toppings() { ID = 203, Name = "Onion", Price = 100 }
+                );
+        }
+    }
+}
